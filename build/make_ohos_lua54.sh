fd92a2d4ffc976d812ce03f8482145624fe134a3
zhenlinyang <zhenlinyang@tencent.com>
2024-05-11 17:29:27 +0800

    Remove -DOHOS_SDK_NATIVE_PLATFORM=${API}

--- build/make_ohos_lua54.sh
+++ build/make_ohos_lua54.sh
@@ -4,14 +4,13 @@ if [ ! -d "$OHOS_SDK_NATIVE" ]; then
 fi
 
 function build() {
-    API=$1
-    ABI=$2
-    TOOLCHAIN_NAME=$3
+    ABI=$1
+    TOOLCHAIN_NAME=$2
     BUILD_PATH=build54.OpenHarmony.${ABI}
-    cmake -H. -B${BUILD_PATH} -DLUA_VERSION=5.4.1 -DOHOS_ARCH=${ABI} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${OHOS_SDK_NATIVE}/build/cmake/ohos.toolchain.cmake -DOHOS_SDK_NATIVE_PLATFORM=${API} -DOHOS_TOOLCHAIN=clang -DOHOS_TOOLCHAIN_NAME=${TOOLCHAIN_NAME}
+    cmake -H. -B${BUILD_PATH} -DLUA_VERSION=5.4.1 -DOHOS_ARCH=${ABI} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${OHOS_SDK_NATIVE}/build/cmake/ohos.toolchain.cmake -DOHOS_TOOLCHAIN=clang -DOHOS_TOOLCHAIN_NAME=${TOOLCHAIN_NAME}
     cmake --build ${BUILD_PATH} --config Release
     mkdir -p plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/
     cp ${BUILD_PATH}/libxlua.so plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/libxlua.so
 }
 
-build ohos-12 arm64-v8a aarch64-linux-ohos
+build arm64-v8a aarch64-linux-ohos
