c9fbe7d8e6e64addea45e3e6e7da9f481905e4f5
zhenlinyang <zhenlinyang@tencent.com>
2024-05-13 12:10:13 +0800

    Build static libs.

--- build/make_android_lua54.sh
+++ build/make_android_lua54.sh
@@ -20,8 +20,8 @@ function build() {
     BUILD_PATH=build54.Android.${ABI}
     cmake -H. -B${BUILD_PATH} -DLUA_VERSION=5.4.1 -DANDROID_ABI=${ABI} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${API} -DANDROID_TOOLCHAIN=clang -DANDROID_TOOLCHAIN_NAME=${TOOLCHAIN_NAME}
     cmake --build ${BUILD_PATH} --config Release
-    mkdir -p plugin_lua54/Plugins/Android/libs/${ABI}/
-    cp ${BUILD_PATH}/libxlua.so plugin_lua54/Plugins/Android/libs/${ABI}/libxlua.so
+#    mkdir -p plugin_lua54/Plugins/Android/libs/${ABI}/
+#    cp ${BUILD_PATH}/libxlua.so plugin_lua54/Plugins/Android/libs/${ABI}/libxlua.so
 }
 
 build android-21 armeabi-v7a arm-linux-androideabi-clang
--- build/make_ohos_lua54.sh
+++ build/make_ohos_lua54.sh
@@ -9,8 +9,8 @@ function build() {
     BUILD_PATH=build54.OpenHarmony.${ABI}
     cmake -H. -B${BUILD_PATH} -DLUA_VERSION=5.4.1 -DOHOS_ARCH=${ABI} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${OHOS_SDK_NATIVE}/build/cmake/ohos.toolchain.cmake -DOHOS_TOOLCHAIN=clang -DOHOS_TOOLCHAIN_NAME=${TOOLCHAIN_NAME}
     cmake --build ${BUILD_PATH} --config Release
-    mkdir -p plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/
-    cp ${BUILD_PATH}/libxlua.so plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/libxlua.so
+#    mkdir -p plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/
+#    cp ${BUILD_PATH}/libxlua.so plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/libxlua.so
 }
 
 build arm64-v8a aarch64-linux-ohos
