eb9848d85f101f1f16878360a41419b933eb3047
zhenlinyang <zhenlinyang@tencent.com>
2025-06-11 01:04:27 +0800

    Update make_win64_lua54_debug.bat

--- build/make_win64_lua54_debug.bat
+++ build/make_win64_lua54_debug.bat
@@ -1,9 +1,9 @@
 
-mkdir build64_54 & pushd build64_54
+mkdir build64_54_debug & pushd build64_54_debug
 cmake -DLUA_VERSION=5.4.1 -G "Visual Studio 17 2022" -A x64  ..
 popd
-cmake --build build64_54 --config Debug
+cmake --build build64_54_debug --config Debug
 md plugin_lua54_debug\Plugins\x86_64
-copy /Y build64_54\Debug\xlua.lib plugin_lua54_debug\Plugins\x86_64\xlua.lib
+copy /Y build64_54_debug\Debug\xlua.lib plugin_lua54_debug\Plugins\x86_64\xlua.lib
 
 pause
